a6832e8a81ed10cbe9dc810b8aa54c9128890400
unknown <shubhamlalranjan@gmail.com>
2021-09-09 02:56:57 +0530

    Changed The Query

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Backend
-  ## Routes
-  ### 1)  /user/getAllModulesByUserId/:userId (http://localhost:9000/user/getAllModulesByUserId/002)
-  ####This route is for getting the Menu of the user
+
+## Routes
+
+### 1) /user/getAllModulesByUserId/:userId (http://localhost:9000/user/getAllModulesByUserId/002)
+
+#### This route is for getting the Menu of the user
+
   <p>first -> we get id of user using userId;</p>
   <p> secound -> we get the menu that your has created;</p>
   <p>third -> we parse the data into disered Structure </p>
--- controllers/userModule.js
+++ controllers/userModule.js
@@ -10,33 +10,42 @@ module.exports.getAllModulesByUserId = async (req, res) => {
   const query = util.promisify(db.query).bind(db);
 
   // Get the User id using userId
-  const user = await query(`SELECT id FROM tbl_user WHERE userId=${userId}`);
+  const user = await query(
+    `SELECT ut.id FROM tbl_user ut WHERE userId=${userId}`
+  );
   if (user.length === 0) {
     // Means user not present with given userId
     return res.status(404).send({ message: "Unable to Find the User" });
   }
 
+  // Creating Query;
+  const QUERY = `select t.moduleName,t.moduleUrl,t.moduleicon,t.parentModuleId,t.moduleType,t.id,max(t.isAuthorized) isAuthorized from ( select distinct tmm.moduleName,tmm.moduleUrl,tmm.moduleIcon,tmm.parentModuleId,tmm.moduleType,tmm.id, case when tg.id is null then 0 else 1 end as isAuthorized from tbl_module_master tmm left join tbl_group_module_rights tgmr on tmm.id=tgmr.moduleId left join (select * from tbl_user_to_group where userid=${user[0].id}) tg on tg.groupId=tgmr.groupId) t group by t.moduleName,t.moduleUrl,t.moduleIcon,t.parentModuleId order by id`;
+
   // Recourds Created By id
-  const menuResult = await query(
-    `SELECT id,moduleName, moduleUrl,moduleIcon,parentModuleId,isActive AS isAuthorized FROM tbl_module_master WHERE createdBy=${user[0].id} ORDER BY parentModuleId ASC`
-  );
+  const menuResult = await query(QUERY);
 
   // Shaping Response
   const parentList = [];
 
-  menuResult.forEach((eachMenu) => {
+  /*
+    Here not using recursion because we have only 2 Level menu
+    Menu -> sub menu
+  */
+  menuResult.forEach(({ moduleType, ...eachMenu }) => {
     eachMenu.key = eachMenu.id;
-    if (eachMenu.parentModuleId === 0) {
+    if (String(moduleType).toLowerCase().trim() === "menu") {
       eachMenu.children = [];
       parentList.push(eachMenu);
     } else {
       parentList.forEach((singleMenu) => {
-        if (singleMenu.id === eachMenu.parentModuleId) {
+        if (eachMenu.parentModuleId === singleMenu.id) {
           singleMenu.children.push(eachMenu);
         }
       });
     }
   });
+
+  // For the SubMenu
 
   return res.status(200).send({
     status: 1,
