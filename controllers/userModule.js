2f947a5f3ed9f4bb703fdbc55ed42ceceb46241f
unknown <shubhamlalranjan@gmail.com>
2021-09-10 02:21:34 +0530

    Changed Algo From forEach to Recursion

--- controllers/userModule.js
+++ controllers/userModule.js
@@ -19,37 +19,45 @@ module.exports.getAllModulesByUserId = async (req, res) => {
   }
 
   // Creating Query;
-  const QUERY = `select t.moduleName,t.moduleUrl,t.moduleicon,t.parentModuleId,t.moduleType,t.id,max(t.isAuthorized) isAuthorized from ( select distinct tmm.moduleName,tmm.moduleUrl,tmm.moduleIcon,tmm.parentModuleId,tmm.moduleType,tmm.id, case when tg.id is null then 0 else 1 end as isAuthorized from tbl_module_master tmm left join tbl_group_module_rights tgmr on tmm.id=tgmr.moduleId left join (select * from tbl_user_to_group where userid=${user[0].id}) tg on tg.groupId=tgmr.groupId) t group by t.moduleName,t.moduleUrl,t.moduleIcon,t.parentModuleId order by id`;
+  const QUERY = `select t.moduleName,t.moduleUrl,t.moduleicon,t.parentModuleId,t.id,max(t.isAuthorized) isAuthorized from ( select distinct tmm.moduleName,tmm.moduleUrl,tmm.moduleIcon,tmm.parentModuleId,tmm.id, case when tg.id is null then 0 else 1 end as isAuthorized from tbl_module_master tmm left join tbl_group_module_rights tgmr on tmm.id=tgmr.moduleId left join (select * from tbl_user_to_group where userid=${user[0].id}) tg on tg.groupId=tgmr.groupId) t group by t.moduleName,t.moduleUrl,t.moduleIcon,t.parentModuleId order by id`;
 
   // Recourds Created By id
   const menuResult = await query(QUERY);
 
-  // Shaping Response
-  const parentList = [];
+  // Achiving tree with recursion process
+  // For the Top Level
+  const topLevel = (data) => {
+    return data.filter((node) => node.parentModuleId === 0);
+  };
 
-  /*
-    Here not using recursion because we have only 2 Level menu
-    Menu -> sub menu
-  */
-  menuResult.forEach(({ moduleType, ...eachMenu }) => {
-    eachMenu.key = eachMenu.id;
-    if (String(moduleType).toLowerCase().trim() === "menu") {
-      eachMenu.children = [];
-      parentList.push(eachMenu);
-    } else {
-      parentList.forEach((singleMenu) => {
-        if (eachMenu.parentModuleId === singleMenu.id) {
-          singleMenu.children.push(eachMenu);
-        }
-      });
+  // Get Single Child
+  const getChildren = (menuList, parentId) => {
+    let children = [];
+    if (parentId == 0) {
+      return children;
     }
-  });
+    menuList.forEach((each) => {
+      if (each.parentModuleId === parentId) {
+        const n_children = getChildren(menuList, each.id);
+        if (n_children.length > 0) {
+          each.children = n_children;
+        }
+        children.push(each);
+      }
+    });
+    return children;
+  };
 
-  // For the SubMenu
+  // Menu Data
+  const menu = topLevel(menuResult).map((each) => {
+    const children = getChildren(menuResult, each.id);
+    if (children.length > 0) each.children = children;
+    return each;
+  });
 
   return res.status(200).send({
     status: 1,
     message: "All module fetched successfully",
-    data: parentList,
+    data: menu,
   });
 };
